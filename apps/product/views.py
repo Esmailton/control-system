3136926dc9b54c193901909c34a7fc79963bbbea
Esmailton Silva <buga_sg12@hotmail.com>
2022-10-11 22:06:06 -0300

    Create View 	Created View to product, category

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,12 +1,28 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
-class Product(View):
+class Product(LoginRequiredMixin, View):
     def get(self, request,id=None):
 
-        return HttpResponse('retrive', status=200)
+        return HttpResponse(status=200)
+
+    def post(self, request):
+
+        return HttpResponse('Created', status=201)
+
+    def put(self, request):
+
+        return HttpResponse('Update', status=200)
+
+
+class Category(LoginRequiredMixin, View):
+    def get(self, request,id=None):
+
+        return HttpResponse(status=200)
 
     def post(self, request):
 
