1a28e4ef0fb2251f12b2839003addb2b3bb61f8c
Esmailton Silva <buga_sg12@hotmail.com>
2022-10-11 22:02:48 -0300

    Test Case Model 	Test case in models Product and Category

--- apps/product/tests/test_models.py
+++ apps/product/tests/test_models.py
@@ -1,4 +1,4 @@
-from apps.product.models import Category
+from apps.product.models import Category, Product
 from django.test import TestCase
 
 
@@ -12,3 +12,24 @@ class CategoryModelTestCase(TestCase):
     def test_category_return_str(self):
         category = Category.objects.get(category_name='Informática')
         self.assertEquals(category.__str__(), 'Informática')
+
+
+class ProductModelTestCase(TestCase):
+    
+    def setUp(self):
+        Category.objects.create(
+            category_name = 'Informática'
+        )
+        category = Category.objects.get(category_name='Informática')
+        Product.objects.create(
+            product_name = 'Mouse',
+            product_price = 100.00,
+            product_category = category,
+            product_photo = None,
+            product_description = 'Mouse sem Fio',
+            product_bar_code = '1323232320123'
+        )
+
+    def test_product_return_str(self):
+        product = Product.objects.get(product_name='Mouse')
+        self.assertEqual(product.__str__(), 'Mouse')
