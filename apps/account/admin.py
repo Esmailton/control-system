c7b27a623573154378df07e43683cd7e92e193af
Esmailton Gomes <contato.esmailtongomes@gmail.com>
2023-07-28 22:58:20 -0300

    Moving code to another file

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,66 +1,8 @@
-from django.contrib import admin
-from django import forms
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-
-
 from apps.account.models import UserCustom
+from .form import UserAdmin
+from django.contrib.auth.models import Group
+from django.contrib import admin
 
-class UserCreationForm(forms.ModelForm):
-    password1=forms.CharField(label=_('Password'), widget=forms.PasswordInput)
-    password2=forms.CharField(
-        label=_('Password confirmation'), widget=forms.PasswordInput
-    )
-
-    class Meta:
-        model=UserCustom
-        fields = "__all__"
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError(_("Passwords must be the same!"))
-        return password2
-    
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        password = self.cleaned_data["password1"]
-        if password:
-            user.set_password(password)
-        if commit:
-            user.save()
-        return user
-
-
-class UserChangeForm(forms.ModelForm):
-
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = UserCustom
-        fields = "__all__"
-
-class UserAdmin(BaseUserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
-
-    list_display = ['username','person', 'date_joined', 'is_active', 'is_admin', 'is_staff','is_admin']
-    list_filter =['is_admin', 'is_active']
-
-    add_fieldsets =[
-        (None, {"fields": ["username", "password1", "password2"]}),
-        ('Personal info', {'fields': ('person',)}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_admin')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    ]
-
-    search_fields = ["username"]
-    ordering = ["username"]
-    filter_horizontal =[]
 
 admin.site.register(UserCustom, UserAdmin)
 admin.site.unregister(Group)
