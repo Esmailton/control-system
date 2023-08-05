1966ff2a0b03ef26142fdc2b20c9ca43aba45328
Esmailton Gomes <contato.esmailtongomes@gmail.com>
2023-08-05 19:46:47 -0300

    refactor(account): Refactoring account manager

--- apps/account/manager.py
+++ apps/account/manager.py
@@ -4,8 +4,6 @@ from django.utils.translation import gettext_lazy as _
 class UserManager(BaseUserManager):
 
     def create_user(self, username, password, **extra_fields):
-        if not username:    
-            raise ValueError(_("User must have username"))
         user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -17,9 +15,4 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("is_admin", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff"))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
         return self.create_user(username, password, **extra_fields)
